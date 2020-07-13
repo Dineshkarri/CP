16dd4fabfa7ae978861ca223398ef5ef4ca8d4eb
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-13 19:28:46 +0530

    Jul 13  19:28:45

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -9,8 +9,8 @@
 import math
 def fun_find_int_roots(a, b, c):
 	dis = b^2 - (4*a*c)
-	root1 = int(-b + math.sqrt(dis) /2*a)
-	root2 = int(-b - math.sqrt(dis) /2*a)
+	root1 = int((-b + math.sqrt(dis)) /(2*a))
+	root2 = int((-b - math.sqrt(dis)) /(2*a))
 	if root1 > root2:
 		return root2, root1
 	return root1, root2
