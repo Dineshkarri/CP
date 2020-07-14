c42c201d7729cdd62500e0d4b553ec60c8ea0031
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-14 15:03:09 +0530

    Jul 14  15:03:09

--- 11-dicetoorderedhand-Python/dicetoorderedhand.py
+++ 11-dicetoorderedhand-Python/dicetoorderedhand.py
@@ -15,7 +15,7 @@ def dicetoorderedhand(a, b, c):
 	# your code goes here
 	x= max(a,b,c)
 	y= min(a,b,c)
-	if a == (b or c):
+	if a == (x or y):
 		return x*100 + a *10 + y
 	if a< x and a>y:
 		return x*100 + a *10 + y
