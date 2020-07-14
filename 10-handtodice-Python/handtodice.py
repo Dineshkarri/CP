99108c6391d80790eb42d731b01fa88f4d534ab4
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-14 13:46:24 +0530

    Jul 14  13:46:23

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -7,4 +7,11 @@
 
 def handtodice(hand):
 	# your code goes here
-	pass
+	while (hand > 0):
+		x = hand % 10
+		hand = hand/10
+		y= hand % 10
+		hand = hand/10
+		z= hand % 10
+		# hand = hand/10
+		return (z,y,x)
