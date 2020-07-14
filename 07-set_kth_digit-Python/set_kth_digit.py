173b273c52c34b88d269fb7855f3bd1f10e4aeed
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-14 21:44:29 +0530

    Jul 14  21:44:29

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -10,7 +10,7 @@ def fun_set_kth_digit(n, k, d):
 		n1 = str(n)
 		n2 =n1[::-1]
 		list1 = list(n2)
-		if k<= len(list1):
+		if k> len(list1):
 			list1.extend(str(d))
 		list1[k] = d
 		n3 = ''.join(map(str,list1))
