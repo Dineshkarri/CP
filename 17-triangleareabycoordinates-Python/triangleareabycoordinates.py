e699418989d53a6f2796e688f66914de7e11ea74
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-14 16:54:08 +0530

    Jul 14  16:54:08

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -4,6 +4,20 @@
 # area of the triangle formed by those three points. Hint: you should make constructive use of
 # the triangleArea function you just wrote above.
 
+import math
+from math import sqrt
+
+def fun_distance(x1, y1, x2, y2):
+	dis = sqrt((y2-y1)**2 + (x2-x1)**2)
+	return (dis)
+
+def trianglearea(s1, s2, s3):
+	s = (s1 +s2 +s3)/2
+	return sqrt(s*(s-s1)*(s-s2)*(s-s3))
+
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a = fun_distance(x1,y1,x3,y3)
+	b = fun_distance(x2,y2,x3,y3)
+	c = fun_distance(x1,y1,x2,y2)
+	return trianglearea(a,b,c)
