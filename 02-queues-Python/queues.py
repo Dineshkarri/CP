e04a322c8170b83f400c684cdb0a16342686ee50
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-15 13:31:15 +0530

    Jul 15  13:31:15

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -5,9 +5,10 @@ few lines as possible.
 Make sure you pass the test cases too!"""
 
 class Queue:
-    list1 = []
+    
     def __init__(self, head=None):
         self.storage = [head]
+        self.list1 = []
 
     def enqueue(self, new_element):
         list1.append(new_element)
