bdf6910b778f74e3d7d5d2d5d0d871fd3956e89c
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-16 22:37:22 +0530

    Jul 16  22:37:21

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -25,6 +25,8 @@ def ishappynumber(n):
 			return True
 
 def isprime(m):
+	if m<=1:
+		return False
 	for i in range (2,m):
 		if m%i ==0:
 			return False
@@ -40,4 +42,4 @@ def fun_nth_happy_prime(n):
 		m +=1
 		if len(list1)==n+1:
 			break
-	return list1[len(list1)]
+	return list1[len(list1)-1]
