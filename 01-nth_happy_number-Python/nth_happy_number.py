cc7f30b4c446c626ff19313a1acaf53346cff1d4
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-16 21:55:11 +0530

    Jul 16  21:55:10

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -10,6 +10,33 @@
 # assert(nthHappyNumber(6) == 28)
 # assert(nthHappyNumber(7) == 31)
 
+def issum(n):
+	sum=num=0
+	while n>0:
+		num = n%10
+		sum =sum + num**2
+		n=n//10
+	return sum
+
+def ishappynumber(n):
+	# your code goes here
+	lis=[]
+	if n<0:
+		return False
+	while True:
+		n = issum(n)
+		if n in lis:
+			return False
+		lis.append(n)
+		if n==1:
+			return True
 
 def fun_nth_happy_number(n):
-	return 0
+	m =1
+	list1=[1]
+	while n>len(list1):
+		if a==ishappynumber(m):
+			list1.append(m)
+		m +=1
+	return list1[n-1]
+		
