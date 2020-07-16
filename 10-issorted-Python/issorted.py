c79f4f00bba94c25ab3b0dcbfa940e7e4c1f584a
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-16 14:10:08 +0530

    Jul 16  14:10:08

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -16,13 +16,13 @@ def issorted(a):
 			print(i)
 			if i == len(a) - 1:
 				break
-			if a[i] <= a[i+1]:
+			if a[i] >= a[i+1]:
 				print('yes')
 				d += 1
 	else:
 		for i in range(len(a)):
 			if i == len(a) - 1:
 				break
-			if a[i] >= a[i+1]:
+			if a[i] <= a[i+1]:
 				d += 1
 	return d==len(a)
