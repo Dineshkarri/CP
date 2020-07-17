6f7f57a4c205d8c63dc78fd5db2e7490f3e58460
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-17 20:03:13 +0530

    Jul 17  20:03:13

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -3,8 +3,18 @@
 # it is also a prime. For example, 313 is a palindrome and it is prime 
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) returns 2
 
-
-
-
+def isprime(m):
+	if m<=1:
+		return False
+	for i in range (2,m):
+		if m%i ==0:
+			return False
+	return True
+	
+def ispd(n):
+	n=str(n)
+	if n[::]==n[::-1]:
+		return True
+	return False
 def fun_nth_palindromic_prime(n):
 	return 0
