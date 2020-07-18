0723b0a34552b15ddb11770dcc18b4345eab3c74
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-18 09:56:43 +0530

    Jul 18  09:56:43

--- 05-alternatingsum-Python/alternatingsum.py
+++ 05-alternatingsum-Python/alternatingsum.py
@@ -10,15 +10,7 @@ def fun_alternatingsum(a):
 	x=0
 	for i in range(0,len(a),2):
 		x=x+a[i]
-	# for i in range(1,len(a)+1,2):
-	# 	y=x+a[i]
-	# return x-y
-	# a=(sum(a[0::2]))
-	# print(a)
 	b=(sum(a[1::2]))
-	print(b)
-	print(a)
-	
-	return a-b
+	return x-b
 
 
