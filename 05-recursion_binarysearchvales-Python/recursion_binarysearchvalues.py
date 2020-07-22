34241ddc550890f8d336acbfbe170933e4531b70
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-22 11:11:09 +0530

    Jul 22  11:11:09

--- 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
+++ 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
@@ -17,7 +17,31 @@
 #     assert(binarySearchValues(L, v) == [(2,'f'), (0,'a'), (1,'c')])
 # Hint: Do not slice the list L, but rather adjust the indexes into L. 
 
+def binary_search(arr, low, high, x): 
+  
+    # Check base case 
+    if high >= low: 
+  
+        mid = (high + low) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+  
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > x: 
+            return binary_search(arr, low, mid - 1, x) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binary_search(arr, mid + 1, high, x) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
+
 def recursion_binarysearchvalues(L, v):
 	# Your codes goes here
-	pass
+	return binary_search([],L, 0,len(L)-1, v)
 	
