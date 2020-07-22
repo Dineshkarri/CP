639b15cd933335c9ece98bfc7c6f01658b54aabe
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-22 13:12:23 +0530

    Jul 22  13:12:23

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -11,10 +11,11 @@ def longestdigitrun(n):
 	l=[]
 	while (n>0):
 		temp=n%10
-		n=n/10
+		n=n//10
 		if n%10 == temp:
 			c+=1
 		else:
 			l.append((temp,c))
 			c=1
-	return l
+	l.sort(key=lambda x:x[1])
+	return l[0][0]
