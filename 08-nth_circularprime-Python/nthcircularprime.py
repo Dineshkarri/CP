1b9caa2df3db1abcf10411dc02a227b2772b514c
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-23 21:38:22 +0530

    Jul 23  21:38:22

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -5,6 +5,13 @@
 # 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime, 
 # note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again).
 
+def isprime(m):
+	if m<=1:
+		return False
+	for i in range (2,m):
+		if m%i ==0:
+			return False
+	return True
 def nthcircularprime(n):
 	# Your code goes here
 	pass
