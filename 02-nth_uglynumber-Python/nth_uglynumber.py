1289faff62832b1db51b321a95e646bd9b048b5f
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-24 14:15:08 +0530

    Jul 24  14:15:08

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -2,6 +2,34 @@
 # Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 
 # 9, 10, 12, 15 shows the few ugly numbers. By convention, nthUglyNumber(0) will give 1.
 
+def primeFactors(n):
+	l=[]
+	m=n
+	while n % 2 == 0: 
+		l.append(2) 
+		n = n // 2
+	for i in range(3,m,2):
+		while n % i== 0:
+			l.append(i)
+			n = n // i
+	if n>2:
+		l.append(n)
+	return l
+def isugly(n):
+	l=primeFactors(n)
+	for i in l:
+		if i!=2 or i!=3 or i!=5:
+			return False
+	return True
 
 def fun_nth_uglynumber(n):
-    return 0
+	if n==0:
+		return 1
+	L=[]
+	j=2
+	while(len(L)<=n):
+		if isugly(j):
+			L.append(j)
+		j+=1
+	print(L)
+	return L[n-1]
