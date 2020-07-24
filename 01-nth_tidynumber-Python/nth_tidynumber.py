52bef7fe3a97defe255b1c9c5ae77ca758f09172
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-24 13:20:15 +0530

    a

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,23 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
+def istidy(n):
+    while(n>0):
+        nu=0
+        num=n%10
+        n=n//10
+        if num < nu:
+            return False
+        nu=num
+    return True
+        
 def fun_nth_tidynumber(n):
+    L=[]
+	j=2
+	while(len(L)<=n):
+		if ispn(j):
+			L.append(j)
+		j+=1
+	print(L)
+	return L[n-1]
     return 0
