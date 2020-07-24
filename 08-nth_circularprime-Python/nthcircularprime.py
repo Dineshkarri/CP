b488f1d4082d1ac0dfca4d0d49b622ac9a99176a
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-24 10:49:24 +0530

    Jul 24  10:49:24

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -12,6 +12,22 @@ def isprime(m):
 		if m%i ==0:
 			return False
 	return True
+
+def iscp(n):
+	n=str(n)
+	m=n+n
+	for i in range(len(n)):
+		if not isprime(int(m[i:i+len(n)])):
+			return False
+	return True
+
 def nthcircularprime(n):
 	# Your code goes here
+	x=2
+	L=[]
+	while(len(L)<=n):
+		if iscp(x):
+			L.append(x)
+	return L[n]
+
 	pass
