24655fbbfe78ccce5c79ba9072fb413466a60226
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-07-28 11:45:57 +0530

    a

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -5,6 +5,34 @@
 # case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either 
 # parameter is not a string, but returns True if both strings are empty (why?).
 
-def samechars(s1, s2):
+def samechars(tup):
 	# Your code goes here
-	pass
+	# if not isinstance(tup[0], str) or not isinstance(tup[1], str): return False
+	# s1,s2 = sorted(list(tup), key=lambda x:len(x))
+
+	# s1Dict={}
+	# for char in s1:
+	# 	s1Dict[char] = True
+
+	# for char in s2:
+	# 	try:
+	# 		check = s1Dict[char]
+	# 	except keyError:
+	# 		return False
+	# 	finally:
+	# 		s1Dict[char] = False
+	if not isinstance(tup[0], str) or not isinstance(tup[1], str):
+		return False
+	s1, s2 = sorted(list(tup), key=lambda x: len(x))
+
+	s1Dict = {}
+	for char in s1:
+		s1Dict[char] = True
+
+	for char in s2:
+		try:
+			check = s1Dict[char]
+		except KeyError:
+			return False
+		finally:
+			s1Dict[char] = False
